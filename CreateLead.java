fedb88f66abcebb7c90cdeee5239cb1b9543bb91
GunaVenkatrao <116972912+GunaVenkatrao@users.noreply.github.com>
2022-11-27 01:04:05 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,24 @@
+package week5.day1class;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+public class CreateLead extends ProjectSpecific {
+@Test
+	public  void runCreateLead() {
+		
+		
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+		
+}
+}
+
+
+
+
+
+
